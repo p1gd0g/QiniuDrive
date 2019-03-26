b176d03a04deb99a3c022a4a762ef1cfea7e4e65
p1gd0g <renhao.szu@gmail.com>
2019-03-26 18:47:54 +0800

    Fix names.

--- main.go
+++ main.go
@@ -15,44 +15,44 @@ func main() {
 		tab := ui.NewTab()
 
 		//tab1
-		button_file1 := ui.NewButton("选择文件")
+		buttonFile1 := ui.NewButton("选择文件")
 		box1 := ui.NewVerticalBox()
 		label1 := ui.NewLabel("未选择文件")
 		label2 := ui.NewLabel("未加密")
 		key1 := ui.NewEntry()
 		key1.SetText("输入密钥")
-		button_enc := ui.NewButton("加密")
-		box1.Append(button_file1, true)
+		buttonEnc := ui.NewButton("加密")
+		box1.Append(buttonFile1, true)
 		box1.Append(label1, false)
 		box1.Append(key1, false)
-		box1.Append(button_enc, true)
+		box1.Append(buttonEnc, true)
 		box1.Append(label2, false)
 		tab.Append("加密", box1)
 
 		//tab2
 		box2 := ui.NewVerticalBox()
-		button_file2 := ui.NewButton("选择文件")
+		buttonFile2 := ui.NewButton("选择文件")
 		label3 := ui.NewLabel("未选择文件")
 		label4 := ui.NewLabel("未解密")
-		button_dec := ui.NewButton("解密")
+		buttonDec := ui.NewButton("解密")
 		key2 := ui.NewEntry()
 		key2.SetText("输入密钥")
-		box2.Append(button_file2, true)
+		box2.Append(buttonFile2, true)
 		box2.Append(label3, false)
 		box2.Append(key2, false)
-		box2.Append(button_dec, true)
+		box2.Append(buttonDec, true)
 		box2.Append(label4, false)
 		tab.Append("解密", box2)
 
 		//tab3
-		button_file3 := ui.NewButton("选择文件")
+		buttonFile3 := ui.NewButton("选择文件")
 		box3 := ui.NewVerticalBox()
 		label5 := ui.NewLabel("未选择文件")
 		label6 := ui.NewLabel("未上传")
-		button_up := ui.NewButton("上传")
-		box3.Append(button_file3, true)
+		buttonUp := ui.NewButton("上传")
+		box3.Append(buttonFile3, true)
 		box3.Append(label5, false)
-		box3.Append(button_up, true)
+		box3.Append(buttonUp, true)
 		box3.Append(label6, false)
 		tab.Append("上传", box3)
 
@@ -77,34 +77,34 @@ func main() {
 		window := ui.NewWindow("Hello", 400, 200, false)
 		window.SetChild(tab)
 
-		button_file1.OnClicked(func(*ui.Button) {
+		buttonFile1.OnClicked(func(*ui.Button) {
 			path = ui.OpenFile(filechooser)
 			label1.SetText("文件地址:" + path)
 
 		})
-		button_enc.OnClicked(func(*ui.Button) {
+		buttonEnc.OnClicked(func(*ui.Button) {
 			file, _ := os.Open(path)
 
 			crypto_p1gd0g.Enc(file, key1.Text())
 			label2.SetText("已加密")
 		})
-		button_file2.OnClicked(func(*ui.Button) {
+		buttonFile2.OnClicked(func(*ui.Button) {
 			path = ui.OpenFile(filechooser)
 			label3.SetText("文件地址:" + path)
 
 		})
-		button_dec.OnClicked(func(*ui.Button) {
+		buttonDec.OnClicked(func(*ui.Button) {
 			file, _ := os.Open(path)
 
 			crypto_p1gd0g.Dec(file, key2.Text())
 			label4.SetText("已解密")
 		})
-		button_file3.OnClicked(func(*ui.Button) {
+		buttonFile3.OnClicked(func(*ui.Button) {
 			path = ui.OpenFile(filechooser)
 			label5.SetText("文件地址:" + path)
 
 		})
-		button_up.OnClicked(func(*ui.Button) {
+		buttonUp.OnClicked(func(*ui.Button) {
 			file.Up(path)
 			var j int
 			for i := 0; i < len(path); i++ {
