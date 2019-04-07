446639ae498a36aaa3d2f1c74a271cc7ac03d942
unknown <2160130433@email.szu.edu.cn>
2019-04-08 01:47:02 +0800

    Refix the size format.

--- tool/formatsize.go
+++ tool/formatsize.go
@@ -4,27 +4,27 @@ import "strconv"
 
 // FormatSize formats the file size.
 func FormatSize(n int64) string {
-	if n < 1000 {
+	if n < 1024 {
 		return strconv.FormatInt(n, 10) + " B"
 	}
 
 	nf := float64(n)
 
-	nf /= 1000
-	if nf < 1000 {
+	nf /= 1024
+	if nf < 1024 {
 		return strconv.FormatFloat(nf, 'f', 2, 64) + " KB"
 	}
 
-	nf /= 1000
-	if nf < 1000 {
+	nf /= 1024
+	if nf < 1024 {
 		return strconv.FormatFloat(nf, 'f', 2, 64) + " MB"
 	}
 
-	nf /= 1000
-	if nf < 1000 {
+	nf /= 1024
+	if nf < 1024 {
 		return strconv.FormatFloat(nf, 'f', 2, 64) + " GB"
 	}
 
-	nf /= 1000
+	nf /= 1024
 	return strconv.FormatFloat(nf, 'f', 2, 64) + " TB"
 }
